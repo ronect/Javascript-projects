010e7e2af13d1f297afd008ba46250f9a21b8bac
ronect <190718519+ronect@users.noreply.github.com>
2026-01-29 05:07:29 -0500

    Update main.js
    Finished mixed messages project

--- Mixed Messages/main.js
+++ Mixed Messages/main.js
@@ -1,35 +1,63 @@
 // Random number generator
-const RandNumGenerator = (num) => (Math.floor(Math.random() * num))
+// Takes a number and returns a random integer from 0 up to (but not including) that number
+const RandNumGenerator = (num) => Math.floor(Math.random() * num);
 
 
-// objects that contain array of different games 
+// Object containing arrays of different game categories
 const GameOptions = {
-    Console : ["Call of duty", "Fortnite", "Fall guys", "Palworld", "Rainbow"],
-    Card : ["Spades", "Poker", "Blackjack", "Rummy", "Uno", "BULL####"],
-    Board : ["Connect 4", "Sorry!", "Trouble", "Guess Who" , "chess", "checkers"]
-}
-
-const GameSelector =() => {
-   const saying = [];
-for (let games in GameOptions){
-    let index = RandNumGenerator(GameOptions[games].length)
-    switch (games){
-        case "Console": saying.push('We can play a Console game like '+ GameOptions[games][index]);
-        break;
-
-        case "Card": saying.push('or we can play a Card game like '+ GameOptions[games][index]);
-        break;
-
-        case "Board": saying.push(" or we can play a Board game like " + GameOptions[games][index]);
-        break;
-
-        default:  console.log('Ask again later');
-        break;
-    
-}
-
-}
-return saying.join(' \n' )}
+    Console: ["Call of duty", "Fortnite", "Fall guys", "Palworld", "Rainbow"],
+    Card: ["Spades", "Poker", "Blackjack", "Rummy", "Uno", "BULL####"],
+    Board: ["Connect 4", "Sorry!", "Trouble", "Guess Who", "chess", "checkers"]
+};
 
 
+// Function that selects a random game from each category
+const GameSelector = () => {
+
+    // Array to store the final output messages
+    const saying = [];
+
+    // Loop through each key in the GameOptions object
+    for (let games in GameOptions) {
+
+        // Generate a random index based on the length of the current game array
+        let index = RandNumGenerator(GameOptions[games].length);
+
+        // Decide what message to add based on the category
+        switch (games) {
+
+            case "Console":
+                // Add a console game suggestion
+                saying.push(
+                    'We can play a Console game like ' + GameOptions[games][index]
+                );
+                break;
+
+            case "Card":
+                // Add a card game suggestion
+                saying.push(
+                    'Or we can play a Card game like ' + GameOptions[games][index]
+                );
+                break;
+
+            case "Board":
+                // Add a board game suggestion
+                saying.push(
+                    'Or we can play a Board game like ' + GameOptions[games][index]
+                );
+                break;
+
+            default:
+                // Runs if none of the cases match (safety fallback)
+                console.log('Ask again later');
+                break;
+        }
+    }
+
+    // Combine all messages into a single string, each on a new line
+    return saying.join(' \n');
+};
+
+
+// Call the function and print the result to the console
 console.log(GameSelector());
